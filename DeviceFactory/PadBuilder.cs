996bbfa5b7326ddc98b6929a28158e43dd9d9d16
ar278 <ar278103@mail.ru>
2023-01-21 18:24:56 +0300

    Добавьте файлы проекта.

---
+++ DeviceFactory/PadBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceFactory
+{
+    public class PadBuilder : DeviceBuilder
+    {
+        public PadBuilder(IDevice device) : base(device)
+        {
+
+        }
+        public override void SetBody(string body)
+        {
+            Device.Body = $"body {body}";
+        }
+
+        public override void SetModel(string model)
+        {
+            Device.Model = $"model {model}";
+        }
+
+        public override void SetName(string name)
+        {
+            Device.Name = $"name {name}";
+        }
+
+        public override void SetScreen(string screen)
+        {
+            Device.Screen = $"screen {screen}";
+        }
+    }
+}
---
+++ DeviceFactory/PhoneBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceFactory
+{
+    public class PhoneBuilder : DeviceBuilder
+    {
+        public PhoneBuilder(IDevice device) : base(device)
+        {
+
+        }
+        public override void SetBody(string body)
+        {
+            Device.Body = $"body {body} ";
+        }
+
+        public override void SetModel(string model)
+        {
+            Device.Model = $"model {model}";
+        }
+
+        public override void SetName(string name)
+        {
+            Device.Name = $"Name: {name} ";
+        }
+
+        public override void SetScreen(string screen)
+        {
+            Device.Screen = $"screen {screen}";
+        }
+    }
+}
